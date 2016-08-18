d93aa39894d9e2d783b6174fd9a97629f3fc126d
Michael O'Hare <mtohare16@gmail.com>
2016-08-18 18:34:36 -0400

    Made changes so if true there is check, if false there is not check

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use('/todos', todosRouter);
 app.use('/', routes);
 app.use('/users', users);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
