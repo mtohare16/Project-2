775df9d923b873d3a9f310ebdab8a1fc0dfff6fa
Michael O'Hare <mtohare16@gmail.com>
2016-08-24 11:48:53 -0400

    Added new MongoLab Sandbox Database

--- app.js
+++ app.js
@@ -17,7 +17,21 @@ var todosRouter = require('./routes/todos');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/movies');
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 
 app.use(logger('dev'));
